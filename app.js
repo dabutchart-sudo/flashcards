f83207916147c4d759ce64aa0bfc91041ff4f318
dabutchart-sudo <dabutchart@googlemail.com>
2025-12-10 20:55:42 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // ===================================================================
 
 // --- 1. PASTE YOUR KEYS HERE (Inside the quotes) ---
-const SUPABASE_URL      = "https://dntitlrtvkgisxwqjxch.supabase.co";
+const SUPABASE_URL = "https://dntitlrtvkgisxwqjxch.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRudGl0bHJ0dmtnaXN4d3FqeGNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxOTQ0MzEsImV4cCI6MjA3OTc3MDQzMX0.S9CXEsHoqp9ATaX23nLI77Q78SvVUSp9V30U-MNcm90";
 const UNSPLASH_ACCESS_KEY = "roF6le_ubyOne6ys-UrkyHpl0afaLEvVeiNOq9ifsnM";
 
